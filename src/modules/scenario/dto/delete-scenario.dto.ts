65b0a02a145a84e3df6358ef8bf71aab510f0390
Mirellacristina <mirela.pereira@estudante.ifms.edu.br>
2025-11-03 11:24:10 -0300

    03/11

--- src/modules/scenario/dto/delete-scenario.dto.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateScenarioDto } from './create-scenario.dto';
-
-export class DeleteScenarioDto extends PartialType(CreateScenarioDto) {}
